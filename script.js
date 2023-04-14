1e25ee08331e10a4650e3019dc4dfdd7d917eea6
Babajide Tella <j.telher@gmail.com>
2023-04-14 19:13:43 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//  THIS IS BEGINNING OF SECTION ONE (JAVASCRIPT) OF THE WEBPAGE
+function show_menu(){
+  var menu = document.getElementById('dropdown');
+
+  if(menu.style.display == 'block'){
+      menu.style.display = 'none';
+  }else {
+      menu.style.display = 'block';                    
+  }
+}
+  //  THIS IS END OF SECTION ONE (JAVASCRIPT) OF THE WEBPAGE
